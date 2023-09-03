d6dcdc3880d53312bd4af0328e2664dbd7e6ed95
Qudus Musiliu <quduusayo4real@gmail.com>
2023-09-03 16:44:23 -0700

    fixes

---
+++ src/constants/index.js
@@ -0,0 +1,103 @@
+
+export const navLinks = [
+    { id: 201, title: 'Home', href: '#hero' },
+    { id: 202, title: 'Services', href: '#service' },
+    { id: 209, title: 'Pricing', href: '#pricing' },
+    { id: 207, title: 'Project', href: '#projects' },
+    { id: 204, title: 'About', href: '#about' },
+    { id: 211, title: 'Contact', href: '#contact' },
+]
+
+
+export const pricings = [
+    {
+        id: 1,
+        packageTitle: 'BASIC',
+        description: 'This package is mainly for small weddings which includes: ',
+        features: ['Video Shootings', 'Video Editing', 'A Flash Drive Containing your work'],
+        price: '#250,000'
+    },
+    {
+        id: 2,
+        packageTitle: 'STANDARD',
+        description: 'This package is mainly for small weddings which includes: ',
+        features: ['Video Shootings', 'Video Editing', 'A Flash Drive containing your work', '360 Video Booth'],
+        price: '#450,000'
+    },
+    {
+        id: 3,
+        packageTitle: 'PREMIUM',
+        description: 'This package is mainly for small weddings which includes: ',
+        features: ['Video Shootings', 'Video Editing', 'A Flash Drive containing your work', '360 Video Booth', 'Drone Footage', 'Pre wedding video for each (Engagement/White)'],
+        price: '#500,000'
+    },
+    {
+        id: 4,
+        packageTitle: 'DIAMOND',
+        description: 'This package is mainly for small weddings which includes: ',
+        features: ['Video Shooting', 'A flash drive containing your work', '360 Video Booth', 'Drone Footage', 'Interviews for the couples, parents and relatives', 'Pre wedding video for each (Engagement/White)'],
+        price: '#600,000'
+    },
+]
+
+
+export const portfolios = [
+    {
+        id: 3,
+        image: 'https://i.postimg.cc/pTpm3y1X/1.png',
+        title: 'WEDDINGS AND PHOTOGRAPHY'
+    },
+    {
+        id: 5,
+        image: 'https://i.postimg.cc/mgsFhLh8/2.png',
+        title: 'WEDDINGS AND PHOTOGRAPHY'
+    },
+    {
+        id: 7,
+        image: 'https://i.postimg.cc/2S7LZsZm/3.png',
+        title: 'MUSIC VIDEOS'
+    },
+    {
+        id: 9,
+        image: 'https://i.postimg.cc/6Q62LMrZ/4.png',
+        title: 'MUSIC VIDEOS'
+    },
+    {
+        id: 11,
+        image: 'https://i.postimg.cc/25HqHnGK/5.png',
+        title: 'LIVE EVENTS'
+    },
+    {
+        id: 13,
+        image: 'https://i.postimg.cc/xChXQYWG/6.png',
+        title: 'LIVE EVENTS'
+    }
+]
+
+export const reasons = [
+    {
+        id: 1,
+        image: 'https://i.postimg.cc/qRZsSPJy/Vector.png',
+        title: 'Timely Delivery',
+        description: 'We deliver your project the exact date agreed by both parties'
+    },
+    {
+        id: 2,
+        image: 'https://i.postimg.cc/bvgHHG46/mdi-video-outline.png',
+        title: 'Professional Equipment',
+        description: 'We ensure that the most advanced equipment are used to carry out your project'
+    },
+    {
+        id: 3,
+        image: 'https://i.postimg.cc/6QtfvVb2/emojione-money-bag.png',
+        title: 'Affordable Prices',
+        description: 'We offer reasonable and affordable prices also i can work with what you have.'
+    },
+    {
+        id: 4,
+        image: 'https://i.postimg.cc/C1KCvhgw/gridicons-video.png',
+        title: 'Quality Videos',
+        description: 'We deliver videos of very high quality that keeps our clients amazed'
+    },
+]
+
